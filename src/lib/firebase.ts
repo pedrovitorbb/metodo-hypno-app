ca4ff28a12b3309644cbd7e9fd1186ba9734402a
pedrovitor baier <pedrovitorbaier027@gmail.com>
2025-07-10 20:23:48 +0000

    oque devo fazer

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCO52NXqG1A7iZOOK96yr1IntIyS3BPffk",
   authDomain: "login-67c98.firebaseapp.com",
   projectId: "login-67c98",
-  storageBucket: "login-67c98.appspot.com",
+  storageBucket: "login-67c98.firebasestorage.app",
   messagingSenderId: "792375254516",
   appId: "1:792375254516:web:36711d82eb2eba9bbeeda1",
   measurementId: "G-SDMK4HN136"
