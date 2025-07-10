155e8e341a193b0b87cf11d7a7112947fdd8a343
pedrovitor baier <pedrovitorbaier027@gmail.com>
2025-07-10 21:11:16 +0000

    quero eles mais centralizados sem a borda sobrando

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -88,7 +88,7 @@ export default function DashboardPage() {
         <div className="flex justify-center">
           <Carousel
             opts={{
-              align: 'start',
+              align: 'center',
               loop: true,
             }}
             className="w-full max-w-6xl"
@@ -96,7 +96,7 @@ export default function DashboardPage() {
             <CarouselContent>
               {modules.map((item, index) => (
                 <CarouselItem key={index} className="basis-full sm:basis-1/2 md:basis-1/2 lg:basis-1/3">
-                  <div className="p-1">
+                  <div className="p-4">
                     <CarouselCard item={item} index={index} />
                   </div>
                 </CarouselItem>
